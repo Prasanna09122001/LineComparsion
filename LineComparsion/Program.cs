13657f27cfe4e609c84856627123f75428e2b154
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-12 11:40:11 +0530

    [Prasanna] Add: Implemented of the Comparsion of the lines

--- LineComparsion/Program.cs
+++ LineComparsion/Program.cs
@@ -9,12 +9,18 @@ namespace LineComparsion
             Console.WriteLine("Welcome to Line Comparsion Problem Statement");
             Operation operation1 = new Operation(2, 1, 4,3);
             double line1 = operation1.CalculateLength();
-            Operation operation2 = new Operation(2, 1, 4, 3);
+            Operation operation2 = new Operation(2, 1, 5, 3);
             double line2 = operation2.CalculateLength();
-            if (line1.Equals(line2))
-                Console.WriteLine("Line1 and Line2 Are Equal");
-            else
-                Console.WriteLine("Line1 and Line2 AreNot Equal");
+            /*   if (line1.Equals(line2))
+                   Console.WriteLine("Line1 and Line2 Are Equal");
+               else
+                   Console.WriteLine("Line1 and Line2 AreNot Equal");*/
+            if (line1.CompareTo(line2) == 0)
+                Console.WriteLine("Both Lines are Equal");
+            if (line1.CompareTo(line2) > 0)
+                Console.WriteLine("Line1 is Greater than Line2");
+            if (line1.CompareTo(line2) < 0)
+                Console.WriteLine("Line2 is Greater than Line1");
 
         }
     }
