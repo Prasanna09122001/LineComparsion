438f418bdc440bb658d4bbe8663e5864917f3b23
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-12 10:46:46 +0530

    [Prasanna] Add: Structure Folder Created

--- LineComparsion/Program.cs
+++ LineComparsion/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LineComparsion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparsion Problem Statement");
+        }
+    }
+}
