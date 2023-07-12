bd0209b317f15fd51a3475be937500a008515186
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-12 12:06:37 +0530

    [Prasanna] Add: Completed of the Line Comparsion Task

--- LineComparsion/Program.cs
+++ LineComparsion/Program.cs
@@ -7,14 +7,17 @@ namespace LineComparsion
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparsion Problem Statement");
+            //UC1
             Operation operation1 = new Operation(2, 1, 4,3);
             double line1 = operation1.CalculateLength();
             Operation operation2 = new Operation(2, 1, 5, 3);
             double line2 = operation2.CalculateLength();
-            /*   if (line1.Equals(line2))
+            //UC2
+            if (line1.Equals(line2))
                    Console.WriteLine("Line1 and Line2 Are Equal");
                else
-                   Console.WriteLine("Line1 and Line2 AreNot Equal");*/
+                   Console.WriteLine("Line1 and Line2 AreNot Equal");
+            //UC3
             if (line1.CompareTo(line2) == 0)
                 Console.WriteLine("Both Lines are Equal");
             if (line1.CompareTo(line2) > 0)
