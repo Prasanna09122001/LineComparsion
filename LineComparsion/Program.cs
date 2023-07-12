6a075f862c6678cdfb55058a9d1c5088edd1ced0
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-12 11:06:20 +0530

    [Prasanna] Add: Implemented of Line Length

--- LineComparsion/Program.cs
+++ LineComparsion/Program.cs
@@ -7,6 +7,9 @@ namespace LineComparsion
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparsion Problem Statement");
+            Operation operation = new Operation(2, 1, 4,3);
+            operation.CalculateLength();
+
         }
     }
 }
