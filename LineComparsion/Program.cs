3e27ff4c171d317f193ca733e0609df3cc3f5537
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-12 11:32:14 +0530

    [Prasanna] Add: Implemented Equality of the line

--- LineComparsion/Program.cs
+++ LineComparsion/Program.cs
@@ -7,8 +7,14 @@ namespace LineComparsion
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparsion Problem Statement");
-            Operation operation = new Operation(2, 1, 4,3);
-            operation.CalculateLength();
+            Operation operation1 = new Operation(2, 1, 4,3);
+            double line1 = operation1.CalculateLength();
+            Operation operation2 = new Operation(2, 1, 4, 3);
+            double line2 = operation2.CalculateLength();
+            if (line1.Equals(line2))
+                Console.WriteLine("Line1 and Line2 Are Equal");
+            else
+                Console.WriteLine("Line1 and Line2 AreNot Equal");
 
         }
     }
